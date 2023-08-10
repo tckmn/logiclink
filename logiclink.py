e0328419ed4fc3787919a0ec9b29b64b7890ce45
Andy Tockman <andy@tck.mn>
2023-08-10 14:28:24 -0400

    images

--- logiclink.py
+++ logiclink.py
@@ -45,7 +45,8 @@ class Table():
 
 conf = Conf(json.load(open('conf.json' if len(sys.argv) < 2 else sys.argv[1])))
 table = Table(conf.table)
-fmt = lambda msg: f'originally posted by **{msg.author.display_name}** https://discord.com/channels/{msg.guild.id}/{msg.channel.id}/{msg.id}\n{msg.content}'
+afmt = lambda a: (lambda s: f'\n{"<"*s}{a.url}{">"*s}')(a.is_spoiler())
+fmt = lambda msg: f'originally posted by **{msg.author.display_name}** https://discord.com/channels/{msg.guild.id}/{msg.channel.id}/{msg.id}\n{msg.content}{"".join(map(afmt, msg.attachments))}'
 
 class LogicLink(discord.Client):
     async def on_ready(self):
